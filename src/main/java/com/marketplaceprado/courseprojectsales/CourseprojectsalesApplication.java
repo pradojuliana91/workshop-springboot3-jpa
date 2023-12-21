30dfa16163b5d1166be0b0c2a5166058037b2ad4
Juliana Prado <ju.pradoo@gmail.com>
2023-12-21 10:35:09 -0300

    Project created

---
+++ src/main/java/com/marketplaceprado/courseprojectsales/CourseprojectsalesApplication.java
@@ -0,0 +1,13 @@
+package com.marketplaceprado.courseprojectsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseprojectsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseprojectsalesApplication.class, args);
+	}
+
+}
