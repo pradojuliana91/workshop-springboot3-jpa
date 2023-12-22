5deede5104357f6f78a8eeee2c58e3e1fad57c25
Juliana Prado <ju.pradoo@gmail.com>
2023-12-22 16:15:59 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/marketplaceprado/courseprojectsales/entities/User.java
+++ src/main/java/com/marketplaceprado/courseprojectsales/entities/User.java
@@ -64,12 +64,14 @@ public class User implements Serializable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         User user = (User) o;
+
         return Objects.equals(id, user.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return id != null ? id.hashCode() : 0;
     }
 }
