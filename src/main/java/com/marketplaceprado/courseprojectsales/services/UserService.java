f7f7caecc23978439c63c7a8e7cb333a513626e6
Juliana Prado <ju.pradoo@gmail.com>
2023-12-26 16:22:12 -0300

    Service layer, component registration

---
+++ src/main/java/com/marketplaceprado/courseprojectsales/services/UserService.java
@@ -0,0 +1,25 @@
+package com.marketplaceprado.courseprojectsales.services;
+
+import com.marketplaceprado.courseprojectsales.entities.User;
+import com.marketplaceprado.courseprojectsales.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
