d129b3e164f3dc588970e1b1d5153a4afcc13bae
Juliana Prado <ju.pradoo@gmail.com>
2024-01-08 16:55:39 -0300

    Payment, one-to-one association

--- src/main/java/com/marketplaceprado/courseprojectsales/entities/Order.java
+++ src/main/java/com/marketplaceprado/courseprojectsales/entities/Order.java
@@ -25,6 +25,8 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -67,6 +69,13 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
