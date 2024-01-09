70f8a2dae382da2e7551b68d9f3f183ac6045b29
Juliana Prado <ju.pradoo@gmail.com>
2024-01-09 14:27:34 -0300

    Exception handling - findById

--- src/main/java/com/marketplaceprado/courseprojectsales/services/UserService.java
+++ src/main/java/com/marketplaceprado/courseprojectsales/services/UserService.java
@@ -2,6 +2,7 @@ package com.marketplaceprado.courseprojectsales.services;
 
 import com.marketplaceprado.courseprojectsales.entities.User;
 import com.marketplaceprado.courseprojectsales.repositories.UserRepository;
+import com.marketplaceprado.courseprojectsales.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class UserService {
     }
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert(User obj) {
         return repository.save(obj);
